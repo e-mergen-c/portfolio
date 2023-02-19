5b7c1d34d1d6c552e356947e21224b7fbf34b36e
dattes git <mergen.ganbaatar@gmail.com>
2023-02-18 21:16:37 -0600

    Fix download function

---
+++ src/components/DownloadResume.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function DownloadResume() {
+
+    const handleDownload = () => {
+		const link = document.createElement('a');
+		link.href = '../mergen-resume-2023.pdf';
+        link.download = 'Mergen_Ganbaatar_Resume.pdf';
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}
+
+    return (
+        <button
+            onClick={handleDownload()}
+            className="inline-block mt-8 px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md "
+        >
+            Download CV
+        </button>
+    )
+}
+
+export default DownloadResume
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DownloadResume from './DownloadResume';
 
 function HeroSection() {
 	return (
@@ -10,12 +11,7 @@ function HeroSection() {
 				<p className="text-md md:text-xl max-w-md mb-8 text-gray-600 dark:text-gray-300">
 					Computer Science student at St Cloud State University{' '}
 				</p>
-				<a
-					href="#works"
-					className="inline-block mt-8 px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-md "
-				>
-					Download CV
-				</a>
+				<DownloadResume />
 			</div>
 		</div>
 	);
